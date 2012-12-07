f5bf1d8f91205ac7ede9df8a9a6a33dfbe92d5ab
Slavey Karadzhov <slavey@zend.com>
2012-12-07 13:01:46 +0100

    Supress errors on delete if the local file is not existing.

--- server/core/provider/storage.py
+++ server/core/provider/storage.py
@@ -145,10 +145,15 @@ class StorageProvider():
         shutil.move(self._translateUri(sourceUri, **kwargs), self._translateUri(targetUri, **kwargs))
 
     def delete(self, uri, **kwargs):
-        if self.isCollection(uri, **kwargs):
-            shutil.rmtree(self._translateUri(uri, **kwargs))
-        else:
-            os.unlink(self._translateUri(uri, **kwargs))
+        try:
+            if self.isCollection(uri, **kwargs):
+                shutil.rmtree(self._translateUri(uri, **kwargs))
+            else:
+                os.unlink(self._translateUri(uri, **kwargs))
+        except OSError as ex:
+            if ex.errno == 2: # Not found
+                return None
+            raise ex
         
         # remove also the views directory
         try:
